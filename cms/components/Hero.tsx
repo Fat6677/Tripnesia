7bbeca77b9dcd9964d55067a0cb4109d6c255510
veriyansyah <veri_yansyah@teknokrat.ac.id>
2026-06-09 01:58:58 +0700

    add code hero

--- cms/components/Hero.tsx
+++ cms/components/Hero.tsx
@@ -12,4 +12,13 @@ export default function Hero() {
       }}
     >
         <div className="absolute inset-0 bg-black/50" />
+
+        <div className="relative z-10 h-full flex flex-col justify-center items-center text-center text-white px-4">
+        <motion.h1
+          initial={{ opacity: 0, y: 80 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="text-5xl md:text-7xl font-bold"
+        >
+          Pantai Setigi Heni
+        </motion.h1>
     </section>

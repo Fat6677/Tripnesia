38c577885ac433971056bac9fab583bd5b362f65
Fat6677 <diobok345@gmail.com>
2026-06-09 14:05:27 +0700

    update: change code main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 9002,
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('Mail Service Running at:\nPort: 9002 (TCP)');
 }
 bootstrap();

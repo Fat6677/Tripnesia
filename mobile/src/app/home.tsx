61f065b32f51d8c307dc6f8e9dd04021519fa1d3
saptaww <sapta_wahyu_tirta@teknokrat.ac.id>
2026-06-09 22:56:12 +0700

    feat: add welcome message on home screen

---
+++ mobile/src/app/home.tsx
@@ -0,0 +1,62 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Tripnesia</Text>
+      <Text style={styles.welcome}>
+        Halo, Traveler 👋
+      </Text>
+
+      <Text style={styles.subtitle}>
+        Destinasi Wisata Populer
+      </Text>
+
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Pantai Setigi Heni</Text>
+        <Text>Kalianda, Lampung Selatan</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Pantai Marina</Text>
+        <Text>Lampung Selatan</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Pantai Embe</Text>
+        <Text>Lampung Selatan</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: '#f1f5f9',
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 15,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  welcome: {
+  fontSize: 16,
+  color: '#64748b',
+  marginTop: 20,
+},
+});

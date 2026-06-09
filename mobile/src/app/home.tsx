57430cb3d9728fa6d89827573fdc6021072327cd
saptaww <sapta_wahyu_tirta@teknokrat.ac.id>
2026-06-10 00:26:44 +0700

    feat: menghapus destinasi pantai embe dari beranda

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -36,11 +36,6 @@ export default function HomeScreen() {
         <Text style={styles.cardTitle}>Pantai Setigi Heni</Text>
         <Text>Kalianda, Lampung Selatan</Text>
       </View>
-
-      <View style={styles.card}>
-        <Text style={styles.cardTitle}>Pantai Embe</Text>
-        <Text>Lampung Selatan</Text>
-      </View>
     </ScrollView>
   );
 }

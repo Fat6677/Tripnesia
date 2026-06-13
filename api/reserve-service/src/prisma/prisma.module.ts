ed65abacc0bd2cd7fad3ba3c613756942c9ce4d1
Fat6677 <diobok345@gmail.com>
2026-06-13 23:54:58 +0700

    update: change code prisma module

--- api/reserve-service/src/prisma/prisma.module.ts
+++ api/reserve-service/src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // 🌟 Membuat PrismaService otomatis bisa diakses di semua modul
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService], // Wajib di-export agar bisa dipakai service lain
 })
 export class PrismaModule {}

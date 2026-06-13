4540ab1cbc60673b2e97a4225256ccb7eefe661e
Fat6677 <diobok345@gmail.com>
2026-06-13 15:32:06 +0700

    fix import app.module

--- api/auth-service/src/auth/auth-module.ts
+++ api/auth-service/src/auth/auth-module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { AuthService } from './auth-service';
+import { AuthController } from './auth-controller';
 import { PrismaModule } from '../prisma/prisma.module'; // Sesuaikan path jika perlu
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';

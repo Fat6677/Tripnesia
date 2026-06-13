43e88adb93e8c2051b2302ba31da109b957ece98
saptaww <sapta_wahyu_tirta@teknokrat.ac.id>
2026-06-13 23:38:56 +0700

    style: memperbarui teks placeholder email

--- user/src/app/register.tsx
+++ user/src/app/register.tsx
@@ -21,7 +21,7 @@ export default function RegisterScreen() {
       />
 
       <TextInput
-        placeholder="Email"
+        placeholder="Masukkan Email"
         style={styles.input}
       />
 

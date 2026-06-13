02a89c5c711f1e2f436f5f9c64a2a772dfd803e1
Fat6677 <diobok345@gmail.com>
2026-06-13 15:07:27 +0700

    update: add export class register

--- api/auth-service/src/auth/dto/auth.dto.ts
+++ api/auth-service/src/auth/dto/auth.dto.ts
@@ -16,3 +16,22 @@ export class LoginDto {
   @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
   password!: string;
 }
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name is required' })
+  name!: string;
+
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
+  password!: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+}

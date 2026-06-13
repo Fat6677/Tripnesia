8f52c528d682bb192751810eccb2f537d7ab53d4
Fat6677 <diobok345@gmail.com>
2026-06-13 23:58:59 +0700

    update: add more reservation feature

--- api/reserve-service/src/reservation/reservation.controller.ts
+++ api/reserve-service/src/reservation/reservation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Body, Param } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 
@@ -6,8 +6,27 @@ import { CreateReservationDto } from './dto/create-reservation.dto';
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
+  // POST http://localhost:8002/api/reservations/book
   @Post('book')
   async createBook(@Body() createReservationDto: CreateReservationDto) {
     return this.reservationService.createReservation(createReservationDto);
+  }
+
+  // GET http://localhost:8002/api/reservations/user/:userId
+  @Get('user/:userId')
+  async getByUser(@Param('userId') userId: string) {
+    return this.reservationService.getReservationsByUser(userId);
+  }
+
+  // GET http://localhost:8002/api/reservations/detail/:id
+  @Get('detail/:id')
+  async getDetail(@Param('id') id: string) {
+    return this.reservationService.getReservationDetail(id);
+  }
+
+  // PATCH http://localhost:8002/api/reservations/cancel/:id
+  @Patch('cancel/:id')
+  async cancelBook(@Param('id') id: string) {
+    return this.reservationService.cancelReservation(id);
   }
 }

b9d1e234c79be56f9706ad7c19d1a115ddd020d6
Fat6677 <diobok345@gmail.com>
2026-06-13 15:07:05 +0700

    update: add esport class dto

--- api/auth-service/src/auth/dto/auth.dto.ts
+++ api/auth-service/src/auth/dto/auth.dto.ts
@@ -5,3 +5,14 @@ import {
   IsString,
   Length,
 } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
+  password!: string;
+}

9347abf445ea8ca01ec830153cb1dc4116944fca
Fat6677 <diobok345@gmail.com>
2026-06-13 15:00:43 +0700

    update: add import for auth-module

---
+++ api/auth-service/src/auth/auth-module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaModule } from '../prisma/prisma.module'; // Sesuaikan path jika perlu
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';

b78a7f41930feaf85f72bc7ad399ec17bd225eea
Fat6677 <diobok345@gmail.com>
2026-06-13 13:34:54 +0700

    feat: reinstall auth-service

--- api/auth-service/src/app.module.ts
+++ api/auth-service/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TestModule } from './test/test.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [TestModule],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- api/auth-service/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // opsional tapi enak biar gak import berkali-kali
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}

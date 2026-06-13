84816f4fdbfdea4bc401f49ac06eec8a15228803
Fat6677 <diobok345@gmail.com>
2026-06-13 13:59:08 +0700

    update: add import for main.ts

--- api/auth-service/src/main.ts
+++ api/auth-service/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Bootstrap-TripnesiaAuth'); // Logger spesifik Tripnesia
 bootstrap();

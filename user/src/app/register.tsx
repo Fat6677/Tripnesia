93bb9268c37023a381ecce0b748904389de337fa
saptaww <sapta_wahyu_tirta@teknokrat.ac.id>
2026-06-13 23:41:08 +0700

    style: memperbarui background halaman registrasi

--- user/src/app/register.tsx
+++ user/src/app/register.tsx
@@ -53,7 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 24,
-    backgroundColor: '#fff',
+    backgroundColor: '#f8fafc',
   },
   title: {
     fontSize: 28,

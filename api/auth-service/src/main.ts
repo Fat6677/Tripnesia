d8c21b1faef26fde6d59a1cbceaf189c8f208b20
Fat6677 <diobok345@gmail.com>
2026-06-13 13:58:44 +0700

    update: add import for main.ts

--- api/auth-service/src/main.ts
+++ api/auth-service/src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
